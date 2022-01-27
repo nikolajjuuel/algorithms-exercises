03d533965fc69fdce7bfd7a44974c2594adf04fd
Nikolaj Johansen <nikolaj.juuel@gmail.com>
2022-01-27 15:40:37 -0800

    recursion exercise

--- specs/recursion/factorials.test.js
+++ specs/recursion/factorials.test.js
@@ -10,7 +10,12 @@
   factorial(3) = 6 
 */
 
-function factorial(n) {}
+function factorial(n) {
+  if (n < 2) return 1;
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
 
 // unit tests
 // do not modify the below code
